ce78cde46689ea2bdc80385294286ace89dfb939
John Huang <johnjunhh@gmail.com>
2017-12-08 23:46:03 -0500

    HUGE changes involving UI

--- js/jjweb.js
+++ js/jjweb.js
@@ -1,7 +1,37 @@
 $(document).ready(function() {
-	//$('#about_btn').click(function(){
+	var projectVisible = false;
+	var aboutVisible = false;
 
-		$('.fade_in').fadeIn(800);
-	//})
+	$('.fade_in').fadeIn(800);
+	$('.footer-basic-centered').fadeIn(800);
+
+	$('#project_btn').click(function(){
+		if(projectVisible){
+	    	$('.project').toggle('drop');
+
+		} else {
+	    	$('.project').css('display', 'inline').delay(10);
+	    	$('.project').hide().delay(10);
+	    	$('.project').toggle('drop').delay(10);
+	    	$('html, body').animate({
+        		scrollTop: $('.project_section').offset().top},
+        		500);
+		}
+		projectVisible = !projectVisible
+	});
+
+	$('#about_btn').click(function(){
+		if(aboutVisible){
+	    	$('.about').toggle('drop');
+
+		} else {
+	    	$('.about').css('display', 'inline').delay(10);
+	    	$('.about').hide().delay(10);
+	    	$('.about').toggle('drop');
+	    	$('html, body').animate({
+        		scrollTop: $('.about_section').offset().top},
+        		500);
+		}
+		aboutVisible = !aboutVisible
+	});
 });
-
